2fd6168d2801eecdbadd4edc24280d0992967eb3
igorsimovski <igor.simovski@valtech.com>
2021-09-09 08:20:27 +0200

    ICSAAS-335 update PR after review  - customize builder for DefaultFulltextSearchRequestPayload  - export classes from core that are necessary for api consumption

--- core/src/main/java/com/valtech/aem/saas/core/fulltextsearch/DefaultFulltextSearchRequestPayload.java
+++ core/src/main/java/com/valtech/aem/saas/core/fulltextsearch/DefaultFulltextSearchRequestPayload.java
@@ -5,8 +5,10 @@ import com.valtech.aem.saas.api.query.FulltextSearchOptionalQuery;
 import com.valtech.aem.saas.api.query.LanguageQuery;
 import com.valtech.aem.saas.api.query.TermQuery;
 import com.valtech.aem.saas.core.query.GetQueryStringConstructor;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import lombok.Builder;
+import lombok.Getter;
 import lombok.NonNull;
 import lombok.Singular;
 
@@ -14,19 +16,26 @@ import lombok.Singular;
  * A default implementation of the full text search payload request. It requires specification of search term and
  * language.
  */
-@Builder(builderMethodName = "hiddenBuilder")
 public class DefaultFulltextSearchRequestPayload implements FulltextSearchGetRequestPayload {
 
+  @Getter
   @NonNull
   private final TermQuery termQuery;
+
+  @Getter
   @NonNull
   private final LanguageQuery languageQuery;
+
+  @Getter
   @Singular
   private final List<FulltextSearchOptionalQuery> optionalQueries;
 
-  public static DefaultFulltextSearchRequestPayloadBuilder builder(TermQuery termQuery,
-      LanguageQuery languageQuery) {
-    return hiddenBuilder().termQuery(termQuery).languageQuery(languageQuery);
+  private DefaultFulltextSearchRequestPayload(@NonNull TermQuery termQuery,
+      @NonNull LanguageQuery languageQuery,
+      List<FulltextSearchOptionalQuery> optionalQueries) {
+    this.termQuery = termQuery;
+    this.languageQuery = languageQuery;
+    this.optionalQueries = optionalQueries;
   }
 
   @Override
@@ -45,5 +54,43 @@ public class DefaultFulltextSearchRequestPayload implements FulltextSearchGetReq
   @Override
   public boolean validate() {
     return !termQuery.getEntries().isEmpty() && !languageQuery.getEntries().isEmpty();
+  }
+
+  public static Builder builder(@NonNull TermQuery termQuery, @NonNull LanguageQuery languageQuery) {
+    return new Builder(termQuery, languageQuery);
+  }
+
+  public static class Builder {
+
+    @NonNull
+    private final TermQuery termQuery;
+    @NonNull
+    private final LanguageQuery languageQuery;
+    private final List<FulltextSearchOptionalQuery> optionalQueries;
+
+    private Builder(@NonNull TermQuery termQuery, @NonNull LanguageQuery languageQuery) {
+      this.termQuery = termQuery;
+      this.languageQuery = languageQuery;
+      this.optionalQueries = new ArrayList<>();
+    }
+
+    public Builder optionalQuery(FulltextSearchOptionalQuery optionalQuery) {
+      this.optionalQueries.add(optionalQuery);
+      return this;
+    }
+
+    public Builder optionalQueries(Collection<? extends FulltextSearchOptionalQuery> optionalQueries) {
+      this.optionalQueries.addAll(optionalQueries);
+      return this;
+    }
+
+    public Builder clearOptionalQueries() {
+      this.optionalQueries.clear();
+      return this;
+    }
+
+    public DefaultFulltextSearchRequestPayload build() {
+      return new DefaultFulltextSearchRequestPayload(this.termQuery, this.languageQuery, this.optionalQueries);
+    }
   }
 }
