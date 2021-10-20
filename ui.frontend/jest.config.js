42f84dab51363ffbc105d92afcc621cd012e8962
Jerome Mottet <jerome.mottet@valtech.com>
2021-10-20 18:22:52 +0200

    ICSAAS-323: Fix Jest + Babel configuration

--- ui.frontend/jest.config.js
+++ ui.frontend/jest.config.js
@@ -172,12 +172,14 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  //transform: {
+  //    "^src.+\\.[jt]sx?$": "babel-jest"
+  //},
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: [
-     "/node_modules/",
-  ],
+  //transformIgnorePatterns: [
+  //   "/node_modules/",
+  //],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
