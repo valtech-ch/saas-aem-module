c58377e9ce7ad03c920387361b1329c35f72e77f
jerome-mottet-valtech <68107513+jerome-mottet-valtech@users.noreply.github.com>
2021-10-22 11:40:38 +0200

    ICSAAS-323: fe base setup (#25)
    * ICSAAS-323: Set up eslint + prettier
    
    * ICSAAS-323: Set up jest
    
    * ICSAAS-323: Fix Jest + Babel configuration
    
    * ICSAAS-323: Add tests sample
    
    * ICSAAS-323: Add lint-staged and husky configuration
    
    * ICSAAS-323: Configure pre-commit hook
    
    * ICSAAS-323: Prevent webpack to load test files in the bundle
    
    * ICSAAS-323: Add initSearch function to bundle
    
    * ICSAAS-323: Update linting configuration with standard Valtech config library

--- ui.frontend/babel.config.js
+++ ui.frontend/babel.config.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
     "presets": [
+        "@babel/preset-env",
         "@babel/typescript"
     ],
     "plugins": [
         "@babel/plugin-proposal-class-properties",
-        "@babel/plugin-proposal-object-rest-spread"
     ]
-}
+};
