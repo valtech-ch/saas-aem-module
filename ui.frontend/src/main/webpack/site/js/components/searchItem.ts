03d182c40cd0ce26c7b618517630486c5aad6c64
Jerome Mottet <jerome.mottet@valtech.com>
2021-11-09 10:12:24 +0100

    ICSAAS-307: Add missing classes to search results item

--- ui.frontend/src/main/webpack/site/js/components/searchItem.ts
+++ ui.frontend/src/main/webpack/site/js/components/searchItem.ts
@@ -11,14 +11,18 @@ export const buildSearchItem = ({
   url,
 }: SearchItem): HTMLDivElement => {
   const searchItem = document.createElement('div')
+  searchItem.classList.add('saas-container_results_item')
 
   const searchItemTitle = document.createElement('h3')
+  searchItemTitle.classList.add('saas-container_results_item_title')
   searchItemTitle.innerHTML = title
 
   const searchItemDescription = document.createElement('p')
+  searchItemDescription.classList.add('saas-container_results_item_description')
   searchItemDescription.innerHTML = description
 
   const searchItemUrl = document.createElement('a')
+  searchItemUrl.classList.add('saas-container_results_item_url')
   searchItemUrl.innerHTML = url
   searchItemUrl.href = url
 
