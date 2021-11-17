56431e7f9c58d8f83c78e9ecb5f2d05b581f2c7a
igorsimovski <89263102+igorsimovski@users.noreply.github.com>
2021-11-17 10:24:34 +0100

    ICSAAS-000 prepare project for mvn central release  - update version to 0.0.1  - added artifact url entry  - exclude ui.tests module  - add license entry  - add developers entry  - update nexus url  - update developerConnection protocol to ssh

--- api/src/main/java/com/valtech/aem/saas/api/query/CompositeFilter.java
+++ api/src/main/java/com/valtech/aem/saas/api/query/CompositeFilter.java
@@ -1,6 +1,5 @@
 package com.valtech.aem.saas.api.query;
 
-import java.util.Collection;
 import java.util.Set;
 import java.util.StringJoiner;
 import lombok.Builder;
@@ -26,37 +25,6 @@ public class CompositeFilter implements Filter {
 
   @Default
   FilterJoinOperator joinOperator = FilterJoinOperator.OR;
-
-  public static ExtendedCompositeFilterBuilder builder() {
-    return new ExtendedCompositeFilterBuilder();
-  }
-
-  public static class ExtendedCompositeFilterBuilder extends CompositeFilterBuilder {
-
-    public ExtendedCompositeFilterBuilder filter(String name, String value) {
-      return filter(new SimpleFilter(name, value));
-    }
-
-    @Override
-    public ExtendedCompositeFilterBuilder filter(Filter filter) {
-      return (ExtendedCompositeFilterBuilder) super.filter(filter);
-    }
-
-    @Override
-    public ExtendedCompositeFilterBuilder joinOperator(FilterJoinOperator joinOperator) {
-      return (ExtendedCompositeFilterBuilder) super.joinOperator(joinOperator);
-    }
-
-    @Override
-    public ExtendedCompositeFilterBuilder clearFilters() {
-      return (ExtendedCompositeFilterBuilder) super.clearFilters();
-    }
-
-    @Override
-    public ExtendedCompositeFilterBuilder filters(Collection<? extends Filter> filters) {
-      return (ExtendedCompositeFilterBuilder) super.filters(filters);
-    }
-  }
 
   @Override
   public String getQueryString() {
