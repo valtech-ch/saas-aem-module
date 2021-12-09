4e6b281706003acfcd10ce07d8bb2cc12e5bfd4a
igorsimovski <igor.simovski@valtech.com>
2021-12-09 11:29:37 +0100

    use Object::hashCode() instead of DigestUtils::sha256Hex

--- core/src/main/java/com/valtech/aem/saas/core/util/ResourceUtil.java
+++ core/src/main/java/com/valtech/aem/saas/core/util/ResourceUtil.java
@@ -1,13 +1,12 @@
 package com.valtech.aem.saas.core.util;
 
 import lombok.NonNull;
-import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public class ResourceUtil {
 
     public static String generateId(@NonNull String prefix, @NonNull String path) {
-        return StringUtils.join((Object[]) new String[]{prefix, "-", StringUtils.substring(DigestUtils.sha256Hex(path),
+        return StringUtils.join((Object[]) new String[]{prefix, "-", StringUtils.substring(String.valueOf(path.hashCode()),
                                                                                            0,
                                                                                            10)});
     }
