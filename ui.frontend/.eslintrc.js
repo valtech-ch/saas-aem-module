b8868fa7f386c7feed57099c79077e35649a3b59
K3v1n <81755864+kevinolivar@users.noreply.github.com>
2022-02-08 10:58:53 +0100

    chore(lint): update eslint config following lint error messages (#122)

--- ui.frontend/.eslintrc.js
+++ ui.frontend/.eslintrc.js
@@ -6,10 +6,13 @@ module.exports = {
     '@valtech-ch/eslint-config/rules',
     'plugin:sonarjs/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2021, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
+    tsconfigRootDir: __dirname,
   },
+  ignorePatterns: ['*.config.js', '.eslintrc.js'],
   rules: {
     'import/no-cycle': 'off',
   },
